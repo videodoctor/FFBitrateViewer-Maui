8adb864b5186b6e21cd90a4cd40d28a4f702e375
Herber Madrigal <hfmad@hotmail.com>
2023-09-12 13:34:30 -0600

    Rename project from FFBitrateViewer to FFBitrateViewer.ApplicationWPF

--- FFBitrateViewer.ApplicationWPF/Properties/Resources.Designer.cs
+++ FFBitrateViewer.ApplicationWPF/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FFBitrateViewer.Properties {
+namespace FFBitrateViewer.ApplicationWPF.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace FFBitrateViewer.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FFBitrateViewer.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FFBitrateViewer.ApplicationWPF.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
