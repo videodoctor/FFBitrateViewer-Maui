17ebaac5c07483aad4ae3b36d41c2b7d5ba2c2ff
Herber Madrigal <hfmad@hotmail.com>
2023-09-13 01:45:13 -0600

    Implement Exit Command

--- FFBitrateViewer.ApplicationAvalonia/ViewModels/MainViewModel.cs
+++ FFBitrateViewer.ApplicationAvalonia/ViewModels/MainViewModel.cs
@@ -1,6 +1,23 @@
-﻿namespace FFBitrateViewer.ApplicationAvalonia.ViewModels;
+﻿using Avalonia.Controls.ApplicationLifetimes;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Windows.Input;
+
+namespace FFBitrateViewer.ApplicationAvalonia.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
     public string Greeting => "Welcome to Avalonia!";
+    public ICommand ExitCommand { get; }
+
+    public MainViewModel()
+    {
+        ExitCommand = new RelayCommand(ExitCommandHandler);
+    }
+
+    private void ExitCommandHandler()
+    {
+        DesktopApplication?.Shutdown();
+    }
+
 }
--- FFBitrateViewer.ApplicationAvalonia/ViewModels/ViewModelBase.cs
+++ FFBitrateViewer.ApplicationAvalonia/ViewModels/ViewModelBase.cs
@@ -1,7 +1,13 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Avalonia.Controls.ApplicationLifetimes;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 
 namespace FFBitrateViewer.ApplicationAvalonia.ViewModels;
 
 public class ViewModelBase : ObservableObject
 {
+    public IClassicDesktopStyleApplicationLifetime? DesktopApplication => _desktopApplication.Value;
+    private static Lazy<IClassicDesktopStyleApplicationLifetime?> _desktopApplication
+        => new Lazy<IClassicDesktopStyleApplicationLifetime?>(() => Avalonia.Application.Current?.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime);
+
 }
