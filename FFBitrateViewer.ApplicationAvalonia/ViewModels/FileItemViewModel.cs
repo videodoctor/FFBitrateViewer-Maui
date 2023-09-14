948a242a31ebca032d0ea76064853c9f843a44e0
Herber Madrigal <hfmad@hotmail.com>
2023-09-13 22:09:21 -0600

    Replace List Box with Data grid to show file list

--- FFBitrateViewer.ApplicationAvalonia/ViewModels/FileItemViewModel.cs
+++ FFBitrateViewer.ApplicationAvalonia/ViewModels/FileItemViewModel.cs
@@ -1,9 +1,17 @@
-﻿using FFBitrateViewer.ApplicationAvalonia.Services;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using FFBitrateViewer.ApplicationAvalonia.Services;
+using System;
 
 namespace FFBitrateViewer.ApplicationAvalonia.ViewModels;
 
-public class FileItemViewModel : ViewModelBase
+public partial class FileItemViewModel : ViewModelBase
 {
+
+    [ObservableProperty]
+    private bool _isSelected;
+
+    public Uri Path { get => _fileEntry.Path;  }
+
     private readonly FileEntry _fileEntry;
 
     public FileItemViewModel(FileEntry fileEntry)
