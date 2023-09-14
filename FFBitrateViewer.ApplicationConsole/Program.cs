4dd1ba78e365ea72ad2ece3390772a926a2b5f27
Herber Madrigal <hfmad@hotmail.com>
2023-09-14 07:09:07 -0600

    Adding console project

---
+++ FFBitrateViewer.ApplicationConsole/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FFBitrateViewer.ApplicationAvalonia.Services;
+
+var oSProcessService = new OSProcessService();
+await oSProcessService.ExecuteAsync(@"echo $env:PATH;", standardOutputWriter: Console.Out);
+//Console.WriteLine("Hello, World!");
