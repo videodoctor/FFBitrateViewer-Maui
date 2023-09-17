646808358fa7afd6dffe357f0203a000770182a5
Herber Madrigal <hfmad@hotmail.com>
2023-09-17 09:58:45 -0600

    Added extension to compute duration

---
+++ FFBitrateViewer.ApplicationAvalonia/Services/ffprobe/FFProbeExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+
+namespace FFBitrateViewer.ApplicationAvalonia.Services.ffprobe;
+
+public static class FFProbeExtensions
+{
+    public static double? GetDuration(this FFProbeJsonOutput ffProbeOutput)
+    {
+        if (ffProbeOutput?.Format?.Duration != null)
+        {
+            return ffProbeOutput.Format.Duration;
+        }
+
+        if (ffProbeOutput.Streams == null)
+        {
+            return default;
+        }
+
+        double? result = (
+            from stream in ffProbeOutput.Streams
+            let duration = stream.Duration
+            let start = stream.StartTime ?? 0
+            where duration != null
+            select duration + start
+            ).FirstOrDefault()
+            ;
+        return result;
+
+    }
+}
