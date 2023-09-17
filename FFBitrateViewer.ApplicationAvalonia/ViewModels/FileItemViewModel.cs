bedcb2df13047ae30873569d3d7e7fa0fd03aa10
hmadrigal <hfmad@hotmail.com>
2023-09-17 15:14:38 -0600

    Add NDPair as record

--- FFBitrateViewer.ApplicationAvalonia/ViewModels/FileItemViewModel.cs
+++ FFBitrateViewer.ApplicationAvalonia/ViewModels/FileItemViewModel.cs
@@ -5,6 +5,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace FFBitrateViewer.ApplicationAvalonia.ViewModels
 {
@@ -81,4 +83,125 @@ namespace FFBitrateViewer.ApplicationAvalonia.ViewModels
     public record SampleRate(int Value) : UInt(Value, Unit.Hertz);
 
 
+    public record NDPair (string Value, int? Numerator, int? Denominator)
+    {
+        public static readonly NDPair Default = new(string.Empty, null, null);
+        private static readonly Regex NDPairRegex  = new(@"^(?<numerator>\d+)/(?<denominator>\d+)$", RegexOptions.IgnoreCase | RegexOptions.Singleline | RegexOptions.Compiled);
+        public static NDPair Parse(string value)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(value, nameof(value));
+
+            var match = NDPairRegex.Match(value);
+
+            if (!match.Success)
+            { throw new FormatException($"Value '{value}' is not in the expected format."); }
+
+            if (!match.Groups.TryGetValue("numerator", out var numeratorGroup))
+            { throw new FormatException($"Value '{value}' is not in the expected format."); }
+
+            if (!match.Groups.TryGetValue("denominator", out var denominatorGroup))
+            { throw new FormatException($"Value '{value}' is not in the expected format."); }
+
+            return new NDPair(value, int.Parse(numeratorGroup.Value), int.Parse(denominatorGroup.Value));
+        }
+
+        public double? ToDouble(){
+            if (Numerator == null || Denominator == null)
+            { return null; }
+            return (double)Numerator / (double)Denominator;
+        }
+
+        public override string ToString()
+        {
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.Append(nameof(NDPair));
+                stringBuilder.Append(" { ");
+                if (PrintMembers(stringBuilder))
+                {
+                    stringBuilder.Append(' ');
+                }
+                stringBuilder.Append('}');
+                stringBuilder.AppendFormat("[ {0} / (1)= {2:F3} ]", Numerator, Denominator, ToDouble());
+                return stringBuilder.ToString();
+        }
+
+    }
+
+    // public abstract record BaseStream
+    // (
+    //     string CodecName,
+    //     string CodecTag,
+    //     string CodecTagString,
+    //     double? Duration,
+    //     long? DurationTS,
+    //     NDPair? FrameRateAvg,
+    //     NDPair? FrameRateR,
+    //     string Id,
+    //     int? Index,
+    //     long? StartPTS,
+    //     double? StartTime,
+    //     NDPair? TimeBase
+    // );
+
+    // public record VideoStreamFormat
+    // (
+    //     bool? Progressive,
+    //     string PixelFormat,
+    //     string ColorSpace,
+    //     string ChromaSubsampling,
+    //     string ColorRange
+    // ){
+    //     public static VideoStreamFormat Build(string colorRange,string pixFmt,string fieldOrder){
+
+
+
+    //         return new VideoStreamFormat
+    //         (
+    //             Progressive: ,
+    //             PixelFormat: pixFmt,
+    //             ColorSpace: ,
+    //             ChromaSubsampling: ,
+    //             ColorRange: colorRange?.ToLower() switch
+    //             {
+    //                 "TV" => colorRange,
+    //                 "PC" => colorRange,
+    //                 _ => null
+    //             }
+    //         );
+    //     }
+    // }
+
+    // public record VideoStream
+    // (
+    //     string CodecName,
+    //     string CodecTag,
+    //     string CodecTagString,
+    //     double? Duration,
+    //     long? DurationTS,
+    //     NDPair? FrameRateAvg,
+    //     NDPair? FrameRateR,
+    //     string Id,
+    //     int? Index,
+    //     long? StartPTS,
+    //     double? StartTime,
+    //     NDPair? TimeBase,
+    //     // Video Properties
+    //     BitRate? BitRate,
+    //     bool IsBitrateCalculated,
+    //     string Encoder,
+
+    // ) : BaseStream(
+    //     CodecName,
+    //     CodecTag,
+    //     CodecTagString,
+    //     Duration,
+    //     DurationTS,
+    //     FrameRateAvg,
+    //     FrameRateR,
+    //     Id,
+    //     Index,
+    //     StartPTS,
+    //     StartTime,
+    //     TimeBase
+    // );
 }
