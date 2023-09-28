1afc4fdc974a377a25a92b2306a51619cd66c92c
Herber Madrigal <hfmad@hotmail.com>
2023-09-27 22:41:20 -0600

    Use single class file format

--- FFBitrateViewer.ApplicationAvalonia.Tests/Services/OSProcessServiceTests.cs
+++ FFBitrateViewer.ApplicationAvalonia.Tests/Services/OSProcessServiceTests.cs
@@ -1,125 +1,123 @@
 ﻿using FFBitrateViewer.ApplicationAvalonia.Services;
-using System.CommandLine.Parsing;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Channels;
 
-namespace FFBitrateViewer.ApplicationAvalonia.Tests.Services
+namespace FFBitrateViewer.ApplicationAvalonia.Tests.Services;
+
+internal class OSProcessServiceTests
 {
-    internal class OSProcessServiceTests
+    private static readonly string TestAppFilePath = typeof(Program).Assembly.Location;
+
+    private OSProcessService _OSProcessService;
+
+    [SetUp]
+    public void Setup()
     {
-        private static readonly string TestAppFilePath = typeof(Program).Assembly.Location;
+        _OSProcessService = new OSProcessService();
+    }
 
-        private OSProcessService _OSProcessService;
+    [Test]
+    public async Task ExecuteCommandWithNoException()
+    {
+        // arrange
+        var command = $"dotnet {TestAppFilePath} echo \"Hello World\"";
 
-        [SetUp]
-        public void Setup()
-        {
-            _OSProcessService = new OSProcessService();
-        }
+        // act
+        var exitCode = await _OSProcessService.ExecuteAsync(command);
 
-        [Test]
-        public async Task ExecuteCommandWithNoException()
-        {
-            // arrange
-            var command = $"dotnet {TestAppFilePath} echo \"Hello World\"";
+        // assert
+        Assert.That(exitCode, Is.EqualTo(0));
+    }
 
-            // act
-            var exitCode = await _OSProcessService.ExecuteAsync(command);
+    [Test]
+    public async Task CaptureUnicodeInStandardOutputWriter()
+    {
+        // arrange
+        var unicodeMessage = "Hello World 🌍 「世界、こんにちは。」";
+        var command = $@"dotnet {TestAppFilePath} echo ""{unicodeMessage}""";
 
-            // assert
-            Assert.That(exitCode, Is.EqualTo(0));
-        }
-
-        [Test]
-        public async Task CaptureUnicodeInStandardOutputWriter()
-        {
-            // arrange
-            var unicodeMessage = "Hello World 🌍 「世界、こんにちは。」";
-            var command = $@"dotnet {TestAppFilePath} echo ""{unicodeMessage}""";
-
-            // act
-            var sb = new StringBuilder();
-            var exitCode = await _OSProcessService.ExecuteAsync(
-                command,
-                standardOutputWriter: new StringWriter(sb),
-                standardOutputEncoding: Encoding.Unicode
-                );
-
-            // assert
-            Assert.That(exitCode, Is.EqualTo(0));
-            Assert.That(sb.ToString(), Is.EqualTo(unicodeMessage));
-        }
-
-        [Test]
-        public async Task CaptureUnicodeInStandardOutputChannel()
-        {
-            // arrange
-            var unicodeMessage = "Hello World 🌍 「世界、こんにちは。」";
-            var command = $@"dotnet {TestAppFilePath} echo ""{unicodeMessage}""";
-
-            // act
-            var commandStdOuputChannel = Channel.CreateUnbounded<string>();
-            var exitCode = await _OSProcessService.ExecuteAsync(
-                command,
-                standardOutputChannel: commandStdOuputChannel,
-                standardOutputEncoding: Encoding.Unicode
+        // act
+        var sb = new StringBuilder();
+        var exitCode = await _OSProcessService.ExecuteAsync(
+            command,
+            standardOutputWriter: new StringWriter(sb),
+            standardOutputEncoding: Encoding.Unicode
             );
 
-            var sb = new StringBuilder();
-            await foreach (var data in commandStdOuputChannel.Reader.ReadAllAsync())
-            {
-                sb.Append(data);
-            }
+        // assert
+        Assert.That(exitCode, Is.EqualTo(0));
+        Assert.That(sb.ToString(), Is.EqualTo(unicodeMessage));
+    }
 
-            // assert
-            Assert.That(exitCode, Is.EqualTo(0));
-            Assert.That(sb.ToString(), Is.EqualTo(unicodeMessage));
+    [Test]
+    public async Task CaptureUnicodeInStandardOutputChannel()
+    {
+        // arrange
+        var unicodeMessage = "Hello World 🌍 「世界、こんにちは。」";
+        var command = $@"dotnet {TestAppFilePath} echo ""{unicodeMessage}""";
 
-        }
+        // act
+        var commandStdOuputChannel = Channel.CreateUnbounded<string>();
+        var exitCode = await _OSProcessService.ExecuteAsync(
+            command,
+            standardOutputChannel: commandStdOuputChannel,
+            standardOutputEncoding: Encoding.Unicode
+        );
 
-        [Test]
-        public async Task ReturnsExitCode()
+        var sb = new StringBuilder();
+        await foreach (var data in commandStdOuputChannel.Reader.ReadAllAsync())
         {
-            // arrange
-            var command = $"dotnet {TestAppFilePath} exit 42";
-
-            // act
-            var exitCode = await _OSProcessService.ExecuteAsync(command);
-
-            // assert
-            Assert.That(exitCode, Is.EqualTo(42));
+            sb.Append(data);
         }
 
-        [Test]
-        public void CancellationProcessStopsProcessing()
-        {
-            // arrange
-            var command = $"dotnet {TestAppFilePath} sleep 60";
-            var cancellationTokenSource = new CancellationTokenSource();
+        // assert
+        Assert.That(exitCode, Is.EqualTo(0));
+        Assert.That(sb.ToString(), Is.EqualTo(unicodeMessage));
 
-            // act
-            var task = _OSProcessService.ExecuteAsync(command, cancellationToken: cancellationTokenSource.Token);
-            cancellationTokenSource.CancelAfter(1000);
-            var taskCanceledException = Assert.ThrowsAsync<TaskCanceledException>(async () => await task);
+    }
 
-            // assert
-            Assert.That(taskCanceledException, Is.Not.Null);
-            Assert.That(task.IsCanceled, Is.True);
-        }
+    [Test]
+    public async Task ReturnsExitCode()
+    {
+        // arrange
+        var command = $"dotnet {TestAppFilePath} exit 42";
 
-        [Test]
-        public void WhichFindDotNetCommand()
-        {
-            // arrange
-            var command = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "dotnet.exe" : "dotnet";
+        // act
+        var exitCode = await _OSProcessService.ExecuteAsync(command);
 
-            // act
-            var foundFilePaths = _OSProcessService.Which(command);
+        // assert
+        Assert.That(exitCode, Is.EqualTo(42));
+    }
 
-            // assert
-            Assert.That(foundFilePaths, Is.Not.Null);
-            Assert.That(foundFilePaths.Any(), Is.True);
-        }
+    [Test]
+    public void CancellationProcessStopsProcessing()
+    {
+        // arrange
+        var command = $"dotnet {TestAppFilePath} sleep 60";
+        var cancellationTokenSource = new CancellationTokenSource();
+
+        // act
+        var task = _OSProcessService.ExecuteAsync(command, cancellationToken: cancellationTokenSource.Token);
+        cancellationTokenSource.CancelAfter(1000);
+        var taskCanceledException = Assert.ThrowsAsync<TaskCanceledException>(async () => await task);
+
+        // assert
+        Assert.That(taskCanceledException, Is.Not.Null);
+        Assert.That(task.IsCanceled, Is.True);
+    }
+
+    [Test]
+    public void WhichFindDotNetCommand()
+    {
+        // arrange
+        var command = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "dotnet.exe" : "dotnet";
+
+        // act
+        var foundFilePaths = _OSProcessService.Which(command);
+
+        // assert
+        Assert.That(foundFilePaths, Is.Not.Null);
+        Assert.That(foundFilePaths.Any(), Is.True);
     }
 }
