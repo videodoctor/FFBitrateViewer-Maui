6c6955eb601bfd506f87c71ed6ca0c4d963017c5
Herber Madrigal <hfmad@hotmail.com>
2023-09-27 22:41:31 -0600

    Add Primitive tests

---
+++ FFBitrateViewer.ApplicationAvalonia.Tests/Extensions/PrimitiveExtensionsTests.cs
@@ -0,0 +1,48 @@
+﻿using FFBitrateViewer.ApplicationAvalonia.Extensions;
+
+namespace FFBitrateViewer.ApplicationAvalonia.Tests.Extensions;
+
+internal class PrimitiveExtensionsTests
+{
+    [Test]
+    public void ToStringWithNullText()
+    {
+        // arrange
+        bool? optionalBoolean = null;
+        string? nullText = "(empty)";
+
+        // act
+        var result = PrimitiveExtensions.ToString(optionalBoolean, nullText: nullText);
+
+        // assert
+        Assert.That(result, Is.EqualTo(nullText));
+    }
+
+    [Test]
+    public void ToStringWithTrueText()
+    {
+        // arrange
+        bool? optionalBoolean = true;
+        string? trueText = "Yes";
+
+        // act
+        var result = PrimitiveExtensions.ToString(optionalBoolean, trueText: trueText);
+
+        // assert
+        Assert.That(result, Is.EqualTo(trueText));
+    }
+
+    [Test]
+    public void ToStringWithFalseText()
+    {
+        // arrange
+        bool? optionalBoolean = false;
+        string? falseText = "No";
+
+        // act
+        var result = PrimitiveExtensions.ToString(optionalBoolean, falseText: falseText);
+
+        // assert
+        Assert.That(result, Is.EqualTo(falseText));
+    }
+}
