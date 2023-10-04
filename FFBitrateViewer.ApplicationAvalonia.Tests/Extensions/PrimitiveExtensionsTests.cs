e3d71d0ea82574b3d8a4fd9e34221469c9e906f2
Herber Madrigal <hfmad@hotmail.com>
2023-10-03 21:11:20 -0600

    Add test ToStringWithNullTextAndTrueText

--- FFBitrateViewer.ApplicationAvalonia.Tests/Extensions/PrimitiveExtensionsTests.cs
+++ FFBitrateViewer.ApplicationAvalonia.Tests/Extensions/PrimitiveExtensionsTests.cs
@@ -45,4 +45,19 @@ internal class PrimitiveExtensionsTests
         // assert
         Assert.That(result, Is.EqualTo(falseText));
     }
+
+    [Test]
+    public void ToStringWithNullTextAndTrueText()
+    {
+        // arrange
+        bool? optionalBoolean = null;
+        string? nullText = "(empty)";
+        string? trueText = "Yes";
+
+        // act
+        var result = PrimitiveExtensions.ToString(optionalBoolean, nullText: nullText, trueText: trueText);
+
+        // assert
+        Assert.That(result, Is.EqualTo(nullText));
+    }
 }
