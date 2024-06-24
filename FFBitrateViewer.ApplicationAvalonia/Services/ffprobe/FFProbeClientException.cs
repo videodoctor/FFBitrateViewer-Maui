350eb63b09fb5dd8218fc95289bca4fa73155215
Herber Madrigal <hfmad@hotmail.com>
2024-06-24 10:24:26 -0600

    Removed reference to deprecated API

--- FFBitrateViewer.ApplicationAvalonia/Services/ffprobe/FFProbeClientException.cs
+++ FFBitrateViewer.ApplicationAvalonia/Services/ffprobe/FFProbeClientException.cs
@@ -8,7 +8,4 @@ public class FFProbeClientException : ApplicationException
     public FFProbeClientException() { }
     public FFProbeClientException(string message) : base(message) { }
     public FFProbeClientException(string message, Exception inner) : base(message, inner) { }
-    protected FFProbeClientException(
-      System.Runtime.Serialization.SerializationInfo info,
-      System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
 }
