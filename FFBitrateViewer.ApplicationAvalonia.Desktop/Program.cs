f632908375b4231b85bea6d9f8f21a5b8b8d44f1
Herber Madrigal <hfmad@hotmail.com>
2024-06-27 20:01:54 -0600

    Adding System.CommandLine in order to accept CLI parameters

--- FFBitrateViewer.ApplicationAvalonia.Desktop/Program.cs
+++ FFBitrateViewer.ApplicationAvalonia.Desktop/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.CommandLine;
+using System.CommandLine.NamingConventionBinder;
+using System.IO;
+using System.Threading.Tasks;
 using Avalonia;
 
 namespace FFBitrateViewer.ApplicationAvalonia.Desktop;
@@ -10,8 +14,53 @@ class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    private static async Task<int> Main(string[] args)
+    {
+        var startTimeAdjustmentOption = new Option<double?>("--StartTimeAdjustment", getDefaultValue: () => null, "Time adjustment when compting the plot");
+        startTimeAdjustmentOption.AddAlias("-s");
+
+        var exitOption = new Option<bool>("--Exit", getDefaultValue: () => false);
+        exitOption.AddAlias("-e");
+
+        var logCommandsOption = new Option<bool>("--LogCommands", getDefaultValue: () => false, "Whether or not to log executed commands");
+        logCommandsOption.AddAlias("-l");
+
+        var autoRunOption = new Option<bool>("--AutoRun", getDefaultValue: () => false, "Whether or not start automatically the file processing.");
+        autoRunOption.AddAlias("-a");
+
+        var tempDirOption = new Option<DirectoryInfo?>("--TempDir", getDefaultValue: () => new DirectoryInfo(Path.GetTempPath()), "Temporary directory");
+        tempDirOption.AddAlias("-t");
+
+        var filesOption = new Option<List<FileInfo>>("--Files", getDefaultValue: () => [], "Input files");
+        filesOption.AddAlias("-f");
+
+        var rootCommand = new RootCommand("Visualizes video bitrate received by ffprobe.exe")
+        {
+            startTimeAdjustmentOption,
+            exitOption,
+            logCommandsOption,
+            autoRunOption,
+            tempDirOption,
+            filesOption,
+        };
+
+        rootCommand.Handler = CommandHandler.Create<double?, bool, bool, bool, string?, string>(
+            (
+                startTimeAdjustmentOptionValue,
+                exitOptionValue,
+                logCommandsOptionValue,
+                autoRunOptionValue,
+                tempDirOptionValue,
+                filesOptionValue
+            ) => StartAvalonia()
+        );
+
+
+        return await rootCommand.InvokeAsync(args);
+    }
+
+    private static int StartAvalonia()
+        => BuildAvaloniaApp().StartWithClassicDesktopLifetime(Environment.GetCommandLineArgs());
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
